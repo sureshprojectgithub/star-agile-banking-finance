4bc999edc09e8c81051ab33edd291e78c8f17c13
sureshprojectgithub <131428396+sureshprojectgithub@users.noreply.github.com>
2023-04-21 20:22:38 +0530

    Update provider.tf

--- deployments/provider.tf
+++ deployments/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "ap-south-1"
-  access_key = "AKIA274V3I3LMA36S55L"
-  secret_key = "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"
+  access_key = "AKIA274V3I3LL4JVPESB"
+  secret_key = "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"
 }
