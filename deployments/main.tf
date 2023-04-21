a156f163c8e5ad5cf78596171ffc26b3720e32b1
suri <alivelusuresh70@gmail.com>
2023-04-21 09:14:46 +0000

    new jenkinsfile and new deployment

---
+++ deployments/main.tf
@@ -0,0 +1,24 @@
+resource "aws_instance" "test-server" {
+  ami           = "ami-0c768662cc797cd75" 
+  instance_type = "t2.micro" 
+  key_name = "manu"
+  vpc_security_group_ids= ["sg-0e578901c39a81e9a"]
+  connection {
+    type     = "ssh"
+    user     = "ec2-user"
+    private_key = file("./bhem.pem")
+    host     = self.public_ip
+  }
+  provisioner "remote-exec" {
+    inline = [ "echo 'wait to start instance' "]
+  }
+  tags = {
+    Name = "test-server"
+  }
+  provisioner "local-exec" {
+        command = " echo ${aws_instance.test-server.public_ip} > inventory "
+		}
+  provisioner "local-exec" {
+  command = " ansible-playbook /var/lib/jenkins/workspace/bankproject/deployments/bankplaybook.yml "
+  } 
+}
---
+++ deployments/provider.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+  region     = "ap-south-1"
+  access_key = "AKIA274V3I3LF5XYZ64G"
+  secret_key = "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"
+}
